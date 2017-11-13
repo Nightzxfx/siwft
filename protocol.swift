62a8cea62b72a38ca690c9f8a657b99ff31a4eb5
Nightzxfx <paulo_valadares@msn.com>
2017-11-13 14:28:50 -0500

    Update protocol.swift

--- protocol.swift
+++ protocol.swift
@@ -1,29 +1,27 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
-
-
-
+ 
+ 
+ 
 protocol TabularDataSource{
-    
+   
     var numberOfRows: Int { get }
     var numberOfColumns: Int { get }
-    
+   
     func label(forcolumn column:Int) -> String
-    
+   
     func itemFor(row: Int, column: Int) -> String
 }
-
-
-
+ 
+ 
+ 
 func printTable(_ dataSource: TabularDataSource)
 {
-    
+   
     var firstRow = "|"
-    
+   
     // Keep track of the width of each column
     var columnWidths = [Int]()
-    
+   
     // Table Header Information
     for i in 0 ..< dataSource.numberOfColumns {
         let columnLabel = dataSource.label(forcolumn: i)
@@ -31,13 +29,13 @@ func printTable(_ dataSource: TabularDataSource)
         firstRow += columnHeader
         columnWidths.append(columnLabel.characters.count)
     }
-    
+   
     print(firstRow)
-    
+   
       for i in 0 ..< dataSource.numberOfRows{
-        
+       
         var out = "|"
-    
+   
         for j in 0 ..< dataSource.numberOfColumns
         {
             let item = dataSource.itemFor(row: i, column: j)
@@ -48,35 +46,35 @@ func printTable(_ dataSource: TabularDataSource)
         print(out)
     }
 }
-
+ 
 struct Person {
     let name: String
     let age: Int
     let yearsOfExperiance: Int
 }
-
+ 
 struct Department: TabularDataSource{
     let name: String
     var people = [Person]()
-    
+   
     init(name: String)
     {
         self.name = name
     }
-    
+   
     mutating func add (_ person: Person)
     {
         people.append(person)
     }
-    
+   
     var numberOfRows: Int{
         return people.count
     }
-    
+   
     var numberOfColumns: Int{
         return 3
     }
-    
+   
     func label(forcolumn column: Int) -> String {
         switch column {
             case 0:
@@ -89,7 +87,7 @@ struct Department: TabularDataSource{
                 fatalError("Invalid Column")
         }
     }
-    
+   
     func itemFor(row: Int, column: Int) -> String {
         let person = people[row]
         switch column {
@@ -100,14 +98,13 @@ struct Department: TabularDataSource{
         }
     }
 }
-
-
-
+ 
+ 
+ 
 var department = Department(name: "Programming")
-
+ 
 department.add(Person(name: "Ali", age: 30, yearsOfExperiance: 10))
 department.add(Person(name: "Steve", age: 32, yearsOfExperiance: 12))
 department.add(Person(name: "Joe", age: 28, yearsOfExperiance: 5))
-
+ 
 printTable(department)
-
